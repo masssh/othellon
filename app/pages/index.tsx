ba1ffae351fa2d292b33f46cd55b8c6244909fa4
Masahiro Yamashita <utm.ver.2.0@gmail.com>
2022-03-06 13:09:42 +0000

    Use next/image vis IPX

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Image from "next/image"
+
 const StaticPage = () => {
   return (
     <div>
       <h1>This page is SSG</h1>
-      <img
+      <Image
         src="https://3.bp.blogspot.com/-hRiScUsWZHk/VA7mVasETMI/AAAAAAAAmOg/OHTyO2Zjxck/s800/othello_game.png"
         alt="othello"
         width={300}
