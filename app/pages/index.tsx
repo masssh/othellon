86f0d5c495a7214ebde17de4bf0292d70b288e2b
Masahiro Yamashita <utm.ver.2.0@gmail.com>
2022-03-06 13:21:17 +0000

    Use ipx

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 
-import Image from "next/image"
 
 const StaticPage = () => {
   return (
     <div>
       <h1>This page is SSG</h1>
-      <Image
-        src="https://3.bp.blogspot.com/-hRiScUsWZHk/VA7mVasETMI/AAAAAAAAmOg/OHTyO2Zjxck/s800/othello_game.png"
+      <img
+        src="https://heuristic-babbage-09414e.netlify.app/.netlify/functions/ipx/w_300,h_300/https://3.bp.blogspot.com/-hRiScUsWZHk/VA7mVasETMI/AAAAAAAAmOg/OHTyO2Zjxck/s800/othello_game.png"
         alt="othello"
         width={300}
         height={300}
