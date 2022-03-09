5c5e8023da02368a462b729ec7a98be50ddc8177
Masahiro Yamashita <utm.ver.2.0@gmail.com>
2022-03-09 14:47:55 +0000

    Do no use netlify

--- app/netlify/functions/ipx.ts
+++
@@ -1,6 +0,0 @@
-import {createIPXHandler} from '@netlify/ipx'
-
-export const handler = createIPXHandler({
-    domains: ["3.bp.blogspot.com"],
-    basePath: "/.netlify/functions/ipx/"
-})
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Image from "next/image"
+
 
 const StaticPage = () => {
   return (
     <div>
       <h1>This page is SSG</h1>
-      <img
+      <Image
         src="https://heuristic-babbage-09414e.netlify.app/.netlify/functions/ipx/w_300,h_300/https://3.bp.blogspot.com/-hRiScUsWZHk/VA7mVasETMI/AAAAAAAAmOg/OHTyO2Zjxck/s800/othello_game.png"
         alt="othello"
         width={300}
